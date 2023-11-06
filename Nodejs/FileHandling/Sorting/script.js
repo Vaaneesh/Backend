e19367a915a6168b9b2ef0638eb989f06116f205
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-11-06 17:54:08 +0530

    modified

--- Nodejs/FileHandling/Sorting/script.js
+++ Nodejs/FileHandling/Sorting/script.js
@@ -3,6 +3,12 @@ let path=require("path");
 let file1=path.join(__dirname,"f1.txt");
 let file2=path.join(__dirname,"f2.txt");
 
+function sort(arr){
+    arr.sort(function(a,b){
+        return a-b;
+    });
+}
+
 function read(file){
     return new Promise((resolve,reject)=>{
         fs.readFile(
@@ -18,10 +24,11 @@ function read(file){
     })
 }
 read(file1).then((data1)=>{
-    console.log(data1);
     read(file2).then((data2)=>{
-        console.log(data2);
         let res=data1+data2;
+        res=res.split("\r\n");
+        console.log(res);
+        sort(res);
         console.log(res);
     })
 })
