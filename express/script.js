2b828d11f2222911e67665ae49183627d1d88b04
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-11-07 17:47:59 +0530

    Changes made in 2 files

--- express/script.js
+++ express/script.js
@@ -1,6 +1,11 @@
 const express=require("express");
 const app=express();
-
-
-
-app.listen(3333);
+app.get("/",(req,res)=>{
+    res.send("Yo sup!!");
+})
+app.get("/about",(req,res)=>{
+    res.send("<h1>About me </h1>")
+})
+app.listen(3333,()=>{
+    console.log("Server Started");
+});
