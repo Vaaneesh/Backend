890c46148924f8bbd3cfe494a1a5e60c9720fab5
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-12-04 17:18:01 +0530

    new file added and changes made

--- serversiderendering/script.js
+++ serversiderendering/script.js
@@ -1,7 +1,18 @@
 let express=require("express");
 let app=express();
 app.set('view engine','hbs');
+var hbs = require('hbs');
+hbs.registerPartials(__dirname + '/views/partials', function (err) {});
 
+let todos=[
+    {name:"cricket",id:1},
+    {name:"hockey",id:2},
+    {name:"football",id:3},
+]
+
+app.get("/",(req,res)=>{
+    res.render("home");
+})
 app.get("/home",(req,res)=>{
     res.render("home");
 })
@@ -11,6 +22,11 @@ app.get("/about",(req,res)=>{
         id:"123"
     });
 
+})
+app.get("/todos",(req,res)=>{
+    res.render("todo",{
+        todos:todos
+    })
 })
 app.listen(3333,()=>{
     console.log("Server started");
